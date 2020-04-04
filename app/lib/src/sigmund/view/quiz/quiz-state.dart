eb408e5656a4de2be6b4f854424898e060a9eacf
RicarteElias <ricarte.elias@outlook.com>
2020-04-03 22:27:26 -0300

    Correção da tela de respostas

--- app/lib/src/sigmund/view/quiz/quiz-state.dart
+++ app/lib/src/sigmund/view/quiz/quiz-state.dart
@@ -10,19 +10,19 @@ class QuizState extends State<QuizPage> with SingleTickerProviderStateMixin{
   final GlobalKey<AnimatedListState> _listKey = GlobalKey();
   //controller da animação
   bool _animationController;
-  //Listas  para animação
+  //Lista para animação
   List<String> _listaAnimada = Questionario().questionario[0]['respostas'];
-  //Listas com as respostas
+  //Lista com as respostas
   List<String> _novasRespostas = Questionario().questionario[1]['respostas'];
   //Texto da pergunta
   String _novaPergunta = Questionario().questionario[0]['pergunta'].toString()+ "..." ;
   //controller da questão
-  int _questaoController = 1;
+  int _questaoController = 0 ;
   //controller pra impedir double tap na lista
   bool _isButtonTapped = false;
+  //lista com o numero de escolhas
+  var qtdeRespostas = [0,0,0,0];
 
-  //parâmetro nao usado
-  get usless => null;
 
 
   @override
@@ -60,23 +60,31 @@ class QuizState extends State<QuizPage> with SingleTickerProviderStateMixin{
   Widget _buildItem(BuildContext context, String item, Animation<double> animation,index) {
     TextStyle textStyle = new TextStyle(fontSize: 22,color: Colors.white,fontWeight: FontWeight.bold);
     return Padding(
-      padding: const EdgeInsets.only(top:20,left: 10,right: 10),
+      padding: const EdgeInsets.only(top:25,left: 10,right: 10),
       child: ScaleTransition(
         scale: animation,
         alignment:_animationController==true?Alignment.centerLeft:Alignment.centerRight,
         child: GestureDetector(
           onTap: (){
-            _isButtonTapped?Null:_proximaPergunta();
+            if(_questaoController == 24){
+              qtdeRespostas[index]++;
+              print(qtdeRespostas);
+              print("ir pra tela final");
+            }
+            else{
+            // ignore: unnecessary_statements
+            _isButtonTapped?null:_proximaPergunta(index);
             setState(() => _isButtonTapped =
-            !_isButtonTapped);
+            !_isButtonTapped);}
+
           },child: SizedBox(
-          height: 90,
-            child:  Container(
-              decoration: BoxDecoration(
+            height: 90,
+              child:  Container(
+                decoration: BoxDecoration(
                 boxShadow: [BoxShadow(color: Color.fromRGBO(68, 31, 84, 1.0), blurRadius: 10,spreadRadius: 3)],
                 borderRadius: BorderRadius.all(Radius.elliptical(10, 10)),color: Constantes.ICON_COLOR,),
-              child: Padding(padding: EdgeInsets.all(10),
-                child:Container(child: Text(item,style: textStyle,)),)
+                child: Padding(padding: EdgeInsets.all(10),
+                  child:Container(child: Text(item,style: textStyle,)),)
           ),
         ),
         )
@@ -99,14 +107,15 @@ class QuizState extends State<QuizPage> with SingleTickerProviderStateMixin{
   }
 
   //Método para chamar a próxima pergunta
-    _proximaPergunta(){
+    _proximaPergunta(index){
+    qtdeRespostas[index]++;
+    print(qtdeRespostas);
     _removerRespotas();
     //Timer para sincronizar a animação da lista
     Timer(Duration(milliseconds: 300), () {
       _adicionarRespostas();
       _isButtonTapped = false;
     });
-
   }
 
   //Método para esvaziar a lista
@@ -116,7 +125,7 @@ class QuizState extends State<QuizPage> with SingleTickerProviderStateMixin{
     for (var i = 0; i < itemCount; i++) {
       String itemToRemove = _listaAnimada[0];
       _listKey.currentState.removeItem(0,
-            (BuildContext context, Animation<double> animation) => _buildItem(context, itemToRemove, animation,usless),
+            (BuildContext context, Animation<double> animation) => _buildItem(context, itemToRemove, animation, null),
         duration: const Duration(milliseconds: 250),
       );
       _listaAnimada.removeAt(0);
